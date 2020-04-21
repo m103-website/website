4ea6e4b67bad1fc9943d43b7064bb3331d78b9b9
Jan Pircher <pircherjan@gmail.com>
2020-04-21 19:58:37 +0200

    Add stuff, not working right now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { JanComponent } from './jan/jan.component';
+import { HomeComponent } from './home/home.component';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JanComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/jan/jan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jan',
+  templateUrl: './jan.component.html',
+  styleUrls: ['./jan.component.css']
+})
+export class JanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
