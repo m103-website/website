0ef916a7da139e8268eba13819ac4c3818a75630
janicscherer <janic@scherer.ml>
2020-05-11 21:04:16 +0200

    fixes 3

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -9,7 +9,6 @@ import {$e} from "codelyzer/angular/styles/chars";
 declare var BeerSlider: BeerSliderScript;
 
 export interface Collection {
-  index: number,
   path: string,
   autor: string,
   pictures: Picture[]
@@ -35,34 +34,32 @@ export class PreviewComponent{
 
   collections: Collection[] = [
     {
-      index: 0,
       path: 'janic',
       autor: 'Janic',
       pictures: [
-        {filename: 'steingross', title: 'Stein gross', iso: '', blende: '', verschlusszeit: '', text: ''},
-        {filename: 'pfosten', title: 'Pfosten', iso: '', blende: '', verschlusszeit: '', text: ''},
-        {filename: 'türstopper', title: 'Türstopper', iso: '', blende: '', verschlusszeit: '', text: ''}
+        {filename: 'steingross', title: 'Stein gross', iso: '100', blende: 'f/4.1', verschlusszeit: '1/80 Sek.', text: 'Auf dem bearbeiteten Bild wurde folgendes erhöht: Sättigung, Tiefen'},
+        {filename: 'pfosten', title: 'Pfosten', iso: '250', blende: 'f/4.6', verschlusszeit: '1/80 Sek.', text: 'Auf dem bearbeiteten Bild wurde folgendes erhöht: Sättigung, Tiefen'},
+        {filename: 'türstopper', title: 'Türstopper', iso: '640', blende: 'f/4.4', verschlusszeit: '1/80 Sek.', text: 'Auf dem bearbeiteten Bild wurde folgendes erhöht: Sättigung, Tiefen, Luminanz'}
         ],
       hasOriginal: false
     },
     {
-      index: 1,
       path: 'jan',
       autor: 'Jan',
       pictures: [
-        {filename: 'schaukel', title: 'Die Schaukel', iso: '320', blende: 'f/4.7', verschlusszeit: '1/80Sek', text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Ich habe die Farbe grün ein wenig kräftiger gemacht, da ich finde, dass diese im originalen Bild zu blass ist. Zusätzlich habe ich die Schattierung in der Kette verändert, durch die etwas dunkleren Farben entstehen mehr Kontraste und die Kette wird interessanter.'},
-        {filename: 'wasserhahn', title: 'Der Wasserhahn', iso: '160', blende: 'f/5.5', verschlusszeit: '1/80 Sek',  text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Auf dem originalen Bild sieht man das Wasser auf dem Wasserhahn fast nicht. Deswegen habe ich beim bearbeiten darauf geschaut, dass die Wassertropfen auf dem Hahn zum Vorschein kommen. Der Rest des Bildes gefällt mir farblich sehr gut, deswegen habe ich versucht den Rest so orignalgetreu wie möglich zu belassen'},
-        {filename: 'zaun', title: 'Der Zaun', iso: '640', blende: 'f/5.1', verschlusszeit: '1/80 Sek',  text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Diese Bild wurde an einem sehr bewöklten Tag aufgenommen. Ich wollte jedoch dass die Stimmung eines sonnigen Tages vermittelt wird, deswegen habe ich das Bild farblich auch sehr stark verändert. Ich habe die Gelb- und Grüntöne im Bild stark hervorgehoben.'}
+        {filename: 'schaukel', title: 'Die Schaukel', iso: '320', blende: 'f/4.7', verschlusszeit: '1/80 Sek.', text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Ich habe die Farbe grün ein wenig kräftiger gemacht, da ich finde, dass diese im originalen Bild zu blass ist. Zusätzlich habe ich die Schattierung in der Kette verändert, durch die etwas dunkleren Farben entstehen mehr Kontraste und die Kette wird interessanter.'},
+        {filename: 'wasserhahn', title: 'Der Wasserhahn', iso: '160', blende: 'f/5.5', verschlusszeit: '1/80 Sek.',  text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Auf dem originalen Bild sieht man das Wasser auf dem Wasserhahn fast nicht. Deswegen habe ich beim bearbeiten darauf geschaut, dass die Wassertropfen auf dem Hahn zum Vorschein kommen. Der Rest des Bildes gefällt mir farblich sehr gut, deswegen habe ich versucht den Rest so orignalgetreu wie möglich zu belassen'},
+        {filename: 'zaun', title: 'Der Zaun', iso: '640', blende: 'f/5.1', verschlusszeit: '1/80 Sek.',  text: 'Auf der linken Seite ist das bearbeitete Bild zu sehen. Diese Bild wurde an einem sehr bewöklten Tag aufgenommen. Ich wollte jedoch dass die Stimmung eines sonnigen Tages vermittelt wird, deswegen habe ich das Bild farblich auch sehr stark verändert. Ich habe die Gelb- und Grüntöne im Bild stark hervorgehoben.'}
         ],
       hasOriginal: true
     },
     {
-      index: 2,
       path: 'marco',
       autor: 'Marco',
       pictures: [
-        {filename: 'steinklein', title: 'Stein klein', iso: '', blende: '', verschlusszeit: '', text: ''},
-        {filename: 'schachfigur', title: 'Schachfigur', iso: '', blende: '', verschlusszeit: '',  text: ''}
+        {filename: 'steinklein', title: 'Stein klein', iso: '80', blende: 'f/5', verschlusszeit: '1/80 Sek.', text: ''},
+        {filename: 'schachfigur', title: 'Schachfigur', iso: '160', blende: 'f/4', verschlusszeit: '1/80 Sek.',  text: ''},
+        {filename: 'hydrant', title: 'Hydrant', iso: '160', blende: 'f/3.9', verschlusszeit: '1/80 Sek.',  text: ''}
       ],
       hasOriginal: false
     }
